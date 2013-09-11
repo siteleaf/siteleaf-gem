761f9cfe611186cebd18174e5f499fac0e303b9c
Skylar Challand <skylar@oakmade.com>
2013-09-11 17:43:37 -0400

    timeout

--- lib/siteleaf/client.rb
+++ lib/siteleaf/client.rb
@@ -1,36 +1,42 @@
 require 'httmultiparty'
 
 module Siteleaf
-  class Client  
+  class Client
     def self.auth(email, password)
       begin
-        request = HTTParty.post(Siteleaf.api_url('auth'), {:basic_auth => {:username => email, :password => password}})
+        request = HTTParty.post(Siteleaf.api_url('auth'), {
+          :basic_auth => {:username => email, :password => password}
+        })
         return request.parsed_response # parse JSON
       rescue => e
         return e.inspect # error
       end
     end
-  
+
     def self.get(path, params = nil)
       self.execute(:get, path, params)
     end
-    
+
     def self.post(path, params)
       self.execute(:post, path, params)
     end
-    
+
     def self.put(path, params)
      self.execute(:put, path, params)
     end
-    
+
     def self.delete(path, params = nil)
       self.execute(:delete, path, params)
     end
-    
+
     def self.execute(method, path, params = nil)
       Siteleaf.load_settings if !Siteleaf.api_key
       begin
-        request = HTTMultiParty.send(method, Siteleaf.api_url(path), {:query => params, :basic_auth => {:username => Siteleaf.api_key, :password => Siteleaf.api_secret}})
+        request = HTTMultiParty.send(method, Siteleaf.api_url(path), {
+          :query => params,
+          :basic_auth => {:username => Siteleaf.api_key, :password => Siteleaf.api_secret},
+          :timeout => 300
+        })
         if request.respond_to?('parsed_response')
           return request.parsed_response # parse JSON
         else
--- lib/siteleaf/entity.rb
+++ lib/siteleaf/entity.rb
@@ -1,24 +1,24 @@
 module Siteleaf
   class Entity
-    
+
     def initialize(attributes = {})
-      self.attributes = attributes    
+      self.attributes = attributes
     end
-    
+
     def self.all
       result = Client.get "#{self.endpoint}"
       result.map { |r| self.new(r) } if result
     end
-    
+
     def self.find(id)
       result = Client.get "#{self.endpoint}/#{id}"
       self.new(result) if result
     end
-    
+
     def self.create(attributes = {})
       self.new(attributes).save
     end
-    
+
     def save
       if self.id
         result = Client.put "#{self.class.endpoint}/#{self.id}", self.attributes
@@ -30,34 +30,34 @@ module Siteleaf
         return self
       end
     end
-    
+
     def self.delete(id)
       Client.delete "#{self.endpoint}/#{id}"
     end
-    
+
     def delete
       Client.delete "#{self.class.endpoint}/#{self.id}"
     end
-    
+
     def attributes
       Hash[self.instance_variables.map { |name| [name[1..-1], self.instance_variable_get(name)] }]
     end
-    
+
     def attributes=(attributes = {})
       attributes.each_pair { |k, v| self.instance_variable_set("@#{k}", v) }
     end
-    
+
     def self.class_name
       self.name.split('::')[-1]
     end
-    
+
     def self.endpoint
       "#{self.class_name.downcase}s"
     end
-    
+
     def create_endpoint
       self.class.endpoint
     end
-    
+
   end
 end
