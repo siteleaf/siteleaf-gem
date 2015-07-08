746cf3e58184a0c3c42ba30d524f57b6bce6be43
amay22 <amay@nyu.edu>
2015-07-08 16:37:16 -0400

    Amended spec_helper require's

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,5 @@
 require File.expand_path('../../lib/siteleaf.rb', __FILE__)
-require File.expand_path('../../lib/siteleaf/asset.rb', __FILE__)
-require File.expand_path('../../lib/siteleaf/client.rb', __FILE__)
-require File.expand_path('../../lib/siteleaf/entity.rb', __FILE__)
-require File.expand_path('../../lib/siteleaf/job.rb', __FILE__)
-require File.expand_path('../../lib/siteleaf/page.rb', __FILE__)
-require File.expand_path('../../lib/siteleaf/post.rb', __FILE__)
-require File.expand_path('../../lib/siteleaf/server.rb', __FILE__)
-require File.expand_path('../../lib/siteleaf/site.rb', __FILE__)
-require File.expand_path('../../lib/siteleaf/theme.rb', __FILE__)
-require File.expand_path('../../lib/siteleaf/user.rb', __FILE__)
-require File.expand_path('../../lib/siteleaf/version.rb', __FILE__)
+Dir["/lib/*.rb"].each { |file| require file }
 require 'vcr'
 require 'webmock/rspec'
 
