882511c5632e7356370c44fea5e98b92936408b8
amay22 <amay@nyu.edu>
2015-07-09 13:01:51 -0400

    Modularized auth Rubocop Fixes

--- bin/authenticate.rb
+++ bin/authenticate.rb
@@ -1,26 +1,34 @@
-def auth(re_auth = false)
-  Siteleaf.load_settings if !re_auth
+def authenticate
+  print 'Enter your Siteleaf e-mail: '
+  email = $stdin.gets.chomp
+  print "Enter your Siteleaf password: \n"
+  system 'stty -echo'
+  password = $stdin.gets.chomp
+  system 'stty echo'
+  Siteleaf::Client.auth(email, password)
+end
 
-  if re_auth or !Siteleaf.api_key
-    print 'Enter your Siteleaf e-mail: '
-    email = $stdin.gets.chomp
-
-    print 'Enter your Siteleaf password: '
-    system 'stty -echo'
-    password = $stdin.gets.chomp
-    system 'stty echo'
-
-    puts "\nAuthorizing..."
-
-    if (auth = Siteleaf::Client.auth(email, password)) && (auth.is_a?(Hash)) && (auth.has_key?('api_key'))
-      File.open(Siteleaf.settings_file,'w') do|file|
-        Marshal.dump({:api_key => auth['api_key'], :api_secret => auth['api_secret']}, file)
-      end
-      puts "=> Gem authorized." if re_auth
-      return true
-    else
-      puts auth['error'] || "Could not authorize, check your e-mail or password."
-      return false
-    end
+def authenticate_write_file(authenticate)
+  File.open(Siteleaf.settings_file, 'w') do |file|
+    Marshal.dump({ api_key: authenticate['api_key'], api_secret: authenticate['api_secret'] }, file)
   end
+  puts '=> Gem authorized.'
+  true
+end
+
+def auth_error(authenticate = nil)
+  puts authenticate.nil? ? 'Could not authorize, check your e-mail or password.' : authenticate['error']
+  false
+end
+
+def authenticate_true(re_auth)
+  Siteleaf.load_settings unless re_auth
+  return true if !re_auth && Siteleaf.api_key
+end
+
+def auth(re_auth = false)
+  return if authenticate_true re_auth
+  authentication =  authenticate
+  return auth_error(authentication) unless authentication.is_a?(Hash) && authentication.key?('api_key')
+  authenticate_write_file(authentication)
 end
