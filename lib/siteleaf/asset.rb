6fb22e1020fee78ccc04b14b485fb4b3e51034c3
amay22 <amay@nyu.edu>
2015-07-09 16:44:52 -0400

    Fixed asset.rb for Rubocop offenses

--- lib/siteleaf/asset.rb
+++ lib/siteleaf/asset.rb
@@ -1,36 +1,35 @@
 module Siteleaf
+  # Class to describe files
   class Asset < Entity
-
     attr_accessor :file, :filename, :replace, :site_id, :page_id, :post_id, :theme_id
     attr_reader :id, :url, :content_type, :filesize, :checksum, :created_at, :updated_at
-    
+
     def create_endpoint
-      if !self.post_id.nil?
-        "posts/#{self.post_id}/assets"
-      elsif !self.page_id.nil?
-        "pages/#{self.page_id}/assets"
-      elsif !self.theme_id.nil?
-        "sites/#{self.site_id}/theme/assets"
+      if !post_id.nil?
+        "posts/#{post_id}/assets"
+      elsif !page_id.nil?
+        "pages/#{page_id}/assets"
+      elsif !theme_id.nil?
+        "sites/#{site_id}/theme/assets"
       else
-        "sites/#{self.site_id}/assets"
+        "sites/#{site_id}/assets"
       end
     end
-    
+
     def post
-      Post.find(self.post_id) if self.post_id
+      Post.find(post_id) if post_id
     end
-    
+
     def page
-      Page.find(self.page_id) if self.page_id
+      Page.find(page_id) if page_id
     end
-    
+
     def theme
-      Theme.find(self.theme_id) if self.theme_id
+      Theme.find(theme_id) if theme_id
     end
-    
+
     def site
-      Site.find(self.site_id) if self.site_id
+      Site.find(site_id) if site_id
     end
-    
   end
 end
