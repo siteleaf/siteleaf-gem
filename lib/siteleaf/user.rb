1ad33118cb19864ebf1803346e3b75cd3c9263b1
amay22 <amay@nyu.edu>
2015-07-09 16:38:33 -0400

    Fixed user.rb for Rubocop offenses

--- lib/siteleaf/user.rb
+++ lib/siteleaf/user.rb
@@ -1,8 +1,7 @@
 module Siteleaf
+  # Stores the Users Attributes for authoriaztion
   class User < Entity
-
     attr_accessor :email, :firstname, :lastname
     attr_reader :id, :created_at, :updated_at
-    
   end
 end
