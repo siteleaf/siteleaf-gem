13cad9b70ede3989c127d4f46d74a80da70c97bc
amay22 <amay@nyu.edu>
2015-07-09 16:39:12 -0400

    Fixed theme.rb for Rubocop offenses

--- lib/siteleaf/theme.rb
+++ lib/siteleaf/theme.rb
@@ -1,25 +1,24 @@
 module Siteleaf
+  # Class to encapsulate the theme files of any siteleaf site
   class Theme < Entity
-
     attr_accessor :name, :site_id
     attr_reader :id, :created_at, :updated_at
-    
+
     def self.find_by_site_id(site_id)
-      Site.new({:id => site_id}).theme
+      Site.new(id: site_id).theme
     end
-    
+
     def self.assets_by_site_id(site_id)
-      Theme.new({:site_id => site_id}).assets
+      Theme.new(site_id: site_id).assets
     end
-    
+
     def site
-      Site.find(self.site_id)
+      Site.find(site_id)
     end
-    
+
     def assets
-      result = Client.get "sites/#{self.site_id}/theme/assets"
-      result.map { |r| Asset.new(r) } if result
+      result = Client.get "sites/#{site_id}/theme/assets"
+      result.map { |asset| Asset.new(asset) } if result
     end
-    
   end
 end
