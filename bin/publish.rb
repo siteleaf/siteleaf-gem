395bd29fa3e056d0afdff22a18a0159dfce3c215
amay22 <amay@nyu.edu>
2015-07-09 13:23:57 -0400

    Modularized publishh Rubocop Fixes

--- bin/publish.rb
+++ bin/publish.rb
@@ -1,17 +1,20 @@
+def do_the_job(job)
+  last_msg = nil
+  job.stream do |s|
+    if (msg = s['message']) && (msg != last_msg)
+      puts msg
+      last_msg = msg
+    end
+  end
+end
+
 def publish(site_id, quiet = true)
   site = Siteleaf::Site.new(id: site_id)
   job = site.publish
-
   if quiet
-    puts "=> Publish queued.\n"
+    puts '=> Publish queued.\n'
   else
-    last_msg = nil
-    job.stream do |s|
-      if (msg = s["message"]) && (msg != last_msg)
-        puts msg
-        last_msg = msg
-      end
-    end
-    puts "=> Publish completed.\n"
+    do_the_job(job)
+    puts '=> Publish completed.\n'
   end
 end
