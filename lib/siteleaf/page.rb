51788adcf2527866655a7b3720240194c107b5d3
amay22 <amay@nyu.edu>
2015-07-09 16:41:52 -0400

    Fixed page.rb for Rubocop offenses

--- lib/siteleaf/page.rb
+++ lib/siteleaf/page.rb
@@ -1,35 +1,38 @@
 module Siteleaf
+  # Encapsulates all Page Attributes and the posts and sub-page in a page
   class Page < Entity
-
     attr_accessor :title, :custom_slug, :body, :visibility, :published_at, :user_id, :site_id, :parent_id, :meta
     attr_reader :id, :slug, :url, :created_at, :updated_at, :assets
-    
+
     def create_endpoint
-      "sites/#{self.site_id}/pages"
+      "sites/#{site_id}/pages"
     end
-    
+
     def site
-      Site.find(self.site_id) if self.site_id
+      Site.find(site_id) if site_id
     end
-    
+
     def assets
-      result = Client.get "pages/#{self.id}/assets"
-      result.map { |r| Asset.new(r) } if result
+      result = fetch_page_items('/assets')
+      result.map { |asset| Asset.new(asset) } if result
     end
-    
+
     def posts
-      result = Client.get "pages/#{self.id}/posts"
-      result.map { |r| Post.new(r) } if result
+      result = fetch_page_items('/posts')
+      result.map { |post| Post.new(post) } if result
     end
-    
+
     def pages
-      result = Client.get "pages/#{self.id}?include=pages"
-      result["pages"].map { |r| Page.new(r) } if result
+      result = fetch_page_items('?include=pages')
+      result['pages'].map { |page| Page.new(page) } if result
     end
-    
+
+    def fetch_page_items(item)
+      Client.get "pages/#{id}" + item
+    end
+
     def page
-      Page.find(self.parent_id) if self.parent_id
+      Page.find(parent_id) if parent_id
     end
-    
   end
 end
