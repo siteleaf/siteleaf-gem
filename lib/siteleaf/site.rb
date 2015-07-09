69819c0a636ff5cc66cd8b636d725c59cd6afb30
amay22 <amay@nyu.edu>
2015-07-09 16:40:38 -0400

    Fixed site.rb for Rubocop offenses

--- lib/siteleaf/site.rb
+++ lib/siteleaf/site.rb
@@ -1,49 +1,51 @@
 module Siteleaf
+  # Encapsulates entore structure of site including page and posts
   class Site < Entity
-
     attr_accessor :title, :domain, :timezone, :meta
     attr_reader :id, :user_id, :created_at, :updated_at
-    
+
     def self.find_by_domain(domain)
-      result = Client.get self.endpoint, {"domain" => domain}
-      self.new(result.first) if result and result.size >= 1
+      result = Client.get endpoint, 'domain' => domain
+      Site.new(result.first) if result && result.size >= 1
     end
-    
+
     def theme
-      @theme ||= if result = Client.get("sites/#{self.id}/theme")
-        theme = Theme.new(result)
-        theme.site_id = self.id
-        theme
-      end
+      result = Client.get("sites/#{id}/theme")
+      theme = Theme.new(result)
+      theme.site_id = id
+      theme
     end
-    
+
     def assets
-      result = Client.get "sites/#{self.id}/assets"
-      result.map { |r| Asset.new(r) } if result
+      result = fetch_site_items('assets')
+      result.map { |asset| Asset.new(asset) } if result
     end
-    
+
     def pages
-      result = Client.get "sites/#{self.id}/pages"
-      result.map { |r| Page.new(r) } if result
+      result = fetch_site_items('pages')
+      result.map { |page| Page.new(page) } if result
     end
-    
+
     def posts
-      result = Client.get "sites/#{self.id}/posts"
-      result.map { |r| Post.new(r) } if result
+      result = fetch_site_items('posts')
+      result.map { |post| Post.new(post) } if result
     end
-    
+
     def resolve(url = '/')
-      Client.get "sites/#{self.id}/resolve", {"url" => url}
+      Client.get "sites/#{id}/resolve", 'url' => url
     end
-    
+
     def preview(url = '/', template = nil)
-      Client.post "sites/#{self.id}/preview", {"url" => url, "template" => template}
+      Client.post "sites/#{id}/preview", 'url' => url, 'template' => template
     end
-    
+
+    def fetch_site_items(item)
+      Client.get "sites/#{id}/" + item
+    end
+
     def publish
-      result = Client.post "sites/#{self.id}/publish", {}
-      Job.new(id: result.parsed_response["job_id"]) if result
+      result = Client.post "sites/#{id}/publish", {}
+      Job.new(id: result.parsed_response['job_id']) if result
     end
-    
   end
 end
