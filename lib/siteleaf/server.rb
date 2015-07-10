b5d920a429df422c696d5a1ea867f17d88e9c748
amay22 <amay@nyu.edu>
2015-07-09 21:22:06 -0400

    Fixed but not Tested server.rb for Rubocop offenses(Errors)

--- lib/siteleaf/server.rb
+++ lib/siteleaf/server.rb
@@ -1,80 +1,110 @@
+require 'uri'
 module Siteleaf
+  # Defines Server
   class Server
-  
     attr_accessor :site_id
-    
+
     def initialize(attributes = {})
-      self.site_id = attributes[:site_id]
+      @site_id = attributes[:site_id]
     end
-    
-    def resolve_template(url = "/")
-      path = url.gsub(/\/\z|\A\//, '') #strip beginning and trailing slashes
-      paths = path.split("/")
+
+    def create_template_if_default_unknown(paths)
       templates = []
-    
-      if ['sitemap.xml','feed.xml'].include?(path)
+      path = paths.join('/')
+      templates << "#{path}.html"
+      templates << "#{path}/index.html"
+      templates << "#{path}/default.html"
+      while paths.size > 0
+        paths.pop
+        templates << "#{paths.join('/')}/default.html" if paths.size > 0
+      end
+      templates
+    end
+
+    def create_template_if_path_vaires(path)
+      templates = []
+      if path == ''
+        templates.push('index.html')
+      else
+        templates = create_template_if_default_unknown(path.split('/'))
+      end
+      templates
+    end
+
+    def resolve_template(url = '/')
+      path = url.gsub(%r{ /\/\z|\A\// }, '') # strip beginning and trailing slashes
+      templates = []
+      if ['sitemap.xml', 'feed.xml'].include?(path)
         templates.push(path)
       else
-        if path == ""
-          templates.push("index.html")
-        else
-          templates.push("#{paths.join('/')}.html")
-          templates.push("#{paths.join('/')}/index.html")
-          templates.push("#{paths.join('/')}/default.html")
-          while paths.size > 0
-            paths.pop
-            templates.push("#{paths.join('/')}/default.html") if paths.size > 0
-          end
-        end
-        templates.push("default.html")
+        templates = create_template_if_path_vaires(path)
+        templates.push('default.html')
       end
-      
-      templates.each do |t|
-        return File.read(t) if File.exist?(t)
-      end
-      
-      return nil
+      templates.each { |template| return File.read(template) if File.exist?(t) }
+      nil
     end
-     
+
+    def asset?(path)
+      /^(?!(sitemap|feed)\.xml)(assets|.*\.)/.match(path)
+    end
+
+    def tags_to_template(template_data)
+      include_tags = %r{ /\{\%\s+include\s+['"]([A-Za-z0-9_\-\/]+)['"]\s+\%\}/ }
+      while include_tags.match(template_data)
+        template_data = template_data.gsub(include_tags) { File.read("_#{Regexp.last_match[1]}.html") }
+      end
+      template_data
+    end
+
+    def output_no_liquid(url, template_data, site)
+      output = site.preview(url, template_data)
+      if output.code == 200 && output.headers['content-type']
+        [output.code, { 'Content-Type' => output.headers['content-type'], 'Content-Length' => output.size.to_s }, [output.to_s]]
+      else
+        response_not_ok(output)
+      end
+    end
+
+    def output_liquid(url, site)
+      output = site.resolve(url)
+      if output.code == 200
+        asset = open(output['file']['url'])
+        [output.code, { 'Content-Type' => asset.content_type, 'Content-Length' => asset.size.to_s }, [asset.read]]
+      else
+        response_not_ok(output)
+      end
+    end
+
+    def response_not_ok(output)
+      [output.code, { 'Content-Type' => 'text/html', 'Content-Length' => output.size.to_s }, [output.to_s]]
+    end
+
+    def compile_liquid(path, url)
+      if (File.exist?("#{path}.liquid") && !(template_data = File.read("#{path}.liquid"))) || (template_data = resolve_template(url))
+        # compile liquid includes into a
+        single page
+        return tags_to_template(template_data, site)
+      end
+      nil
+    end
+
+    def site_exist?(site, path)
+      if asset(path) && !File.exist?("#{path}.liquid")
+        output_liquid(url, site)
+      else
+        compile_liquid(path, url)
+        output_no_liquid(url, template_data, site)
+      end
+    end
+
     def call(env)
-      require 'uri'
-      
-      site = Siteleaf::Site.new({:id => self.site_id})
-      
+      site = Siteleaf::Site.new(id: site_id)
       url = URI.unescape(env['PATH_INFO'])
-      path = url.gsub(/\/\z|\A\//, '') #strip beginning and trailing slashes
-      
-      if !['sitemap.xml','feed.xml'].include?(path) and !File.directory?(path) and File.exist?(path)
+      path = url.gsub(%r{ /\/\z|\A\// }, '') # strip beginning and trailing slashes
+      if !['sitemap.xml', 'feed.xml'].include?(path) && !File.directory?(path) && File.exist?(path)
         Rack::File.new(Dir.pwd).call(env)
       else
-        template_data = nil
-        is_asset = /^(?!(sitemap|feed)\.xml)(assets|.*\.)/.match(path)
-        
-        if is_asset and !File.exist?("#{path}.liquid")
-          output = site.resolve(url)
-          if output.code == 200
-            require 'open-uri'
-            asset = open(output['file']['url'])
-            [output.code, {'Content-Type' => asset.content_type, 'Content-Length' => asset.size.to_s}, [asset.read]]
-          else
-            [output.code, {'Content-Type' => 'text/html', 'Content-Length' => output.size.to_s}, [output.to_s]]
-          end
-        else
-          if (File.exist?("#{path}.liquid") and template_data = File.read("#{path}.liquid")) or (template_data = resolve_template(url))
-            # compile liquid includes into a single page
-            include_tags = /\{\%\s+include\s+['"]([A-Za-z0-9_\-\/]+)['"]\s+\%\}/
-            while include_tags.match(template_data)
-              template_data = template_data.gsub(include_tags) { |i| File.read("_#{$1}.html") }
-            end
-          end
-        
-          output = site.preview(url, template_data)
-          if output.code == 200 && output.headers["content-type"]
-            [output.code, {'Content-Type' => output.headers["content-type"], 'Content-Length' => output.size.to_s}, [output.to_s]]
-          else
-            [output.code, {'Content-Type' => 'text/html', 'Content-Length' => output.size.to_s}, [output.to_s]]
-          end
-        end
+        site_exist?(site, path)
       end
     end
   end
