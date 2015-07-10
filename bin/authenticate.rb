82a173802e93efc2edbb8b3aab0a4f6e47032041
amay22 <amay@nyu.edu>
2015-07-10 09:47:41 -0400

    Added Authenticate Module and fixed module method call

--- bin/authenticate.rb
+++ bin/authenticate.rb
@@ -1,34 +1,39 @@
-def authenticate
-  print 'Enter your Siteleaf e-mail: '
-  email = $stdin.gets.chomp
-  print "Enter your Siteleaf password: \n"
-  system 'stty -echo'
-  password = $stdin.gets.chomp
-  system 'stty echo'
-  Siteleaf::Client.auth(email, password)
-end
+module SiteleafCommands
+  # Provides functions for each User Authentication
+  module Authenticate
+    def authenticate
+      print 'Enter your Siteleaf e-mail: '
+      email = $stdin.gets.chomp
+      print "Enter your Siteleaf password: \n"
+      system 'stty -echo'
+      password = $stdin.gets.chomp
+      system 'stty echo'
+      Siteleaf::Client.auth(email, password)
+    end
 
-def authenticate_write_file(authenticate)
-  File.open(Siteleaf.settings_file, 'w') do |file|
-    Marshal.dump({ api_key: authenticate['api_key'], api_secret: authenticate['api_secret'] }, file)
+    def authenticate_write_file(authenticate)
+      File.open(Siteleaf.settings_file, 'w') do |file|
+        Marshal.dump({ api_key: authenticate['api_key'], api_secret: authenticate['api_secret'] }, file)
+      end
+      puts '=> Gem authorized.'
+      true
+    end
+
+    def auth_error(authenticate = nil)
+      puts authenticate.nil? ? 'Could not authorize, check your e-mail or password.' : authenticate['error']
+      false
+    end
+
+    def authenticate_true(re_auth)
+      Siteleaf.load_settings unless re_auth
+      return true if !re_auth && Siteleaf.api_key
+    end
+
+    def auth(re_auth = false)
+      return if authenticate_true re_auth
+      authentication =  authenticate
+      return auth_error(authentication) unless authentication.is_a?(Hash) && authentication.key?('api_key')
+      authenticate_write_file(authentication)
+    end
   end
-  puts '=> Gem authorized.'
-  true
-end
-
-def auth_error(authenticate = nil)
-  puts authenticate.nil? ? 'Could not authorize, check your e-mail or password.' : authenticate['error']
-  false
-end
-
-def authenticate_true(re_auth)
-  Siteleaf.load_settings unless re_auth
-  return true if !re_auth && Siteleaf.api_key
-end
-
-def auth(re_auth = false)
-  return if authenticate_true re_auth
-  authentication =  authenticate
-  return auth_error(authentication) unless authentication.is_a?(Hash) && authentication.key?('api_key')
-  authenticate_write_file(authentication)
 end
