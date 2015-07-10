c7ec49abf8675d2af0e5e81be3f4f414db3063fb
amay22 <amay@nyu.edu>
2015-07-10 10:37:21 -0400

    Fixed module Push & Pull functions call by prefixing  self.

--- bin/pull.rb
+++ bin/pull.rb
@@ -1,21 +1,21 @@
 module SiteleafCommands
   # Provides get_theme_assets functions for sitleaf Pull command
   module Pull
-    def read_asset_server(asset_path)
+    def self.read_asset_server(asset_path)
       open(asset_path, /^1\.8/.match(RUBY_VERSION) ? 'r' : 'r:UTF-8') { |f| f.read }
     end
 
-    def write_asset_local(asset_filename, file)
+    def self.write_asset_local(asset_filename, file)
       FileUtils.mkdir_p(File.dirname(asset_filename))
       File.open(asset_filename, /^1\.8/.match(RUBY_VERSION) ? 'w' : 'w:UTF-8') { |f| f.write(file) }
     end
 
-    def asset_exists_and_checksum(asset)
+    def self.asset_exists_and_checksum(asset)
       # checksum is to tell us if the site has already been updated or not.
       File.exist?(asset.filename) && (asset.checksum == Digest::MD5.hexdigest(File.read(asset.filename)))
     end
 
-    def get_theme_assets(site_id)
+    def self.get_theme_assets(site_id)
       return if (assets = Siteleaf::Theme.assets_by_site_id(site_id)).nil?
       updated_count = 0
       assets.each do |asset|
