644e760fd0d7299949c8cf8ba0393208b9a7f215
amay22 <amay@nyu.edu>
2015-07-10 10:12:19 -0400

    Added Publish Module to SiteLeafCommands

--- bin/publish.rb
+++ bin/publish.rb
@@ -1,20 +1,25 @@
-def do_the_job(job)
-  last_msg = nil
-  job.stream do |s|
-    if (msg = s['message']) && (msg != last_msg)
-      puts msg
-      last_msg = msg
+module SiteleafCommands
+  # Provides Publish functions for sitleaf publish command
+  module Publish
+    def do_the_job(job)
+      last_msg = nil
+      job.stream do |s|
+        if (msg = s['message']) && (msg != last_msg)
+          puts msg
+          last_msg = msg
+        end
+      end
     end
-  end
-end
 
-def publish(site_id, quiet = true)
-  site = Siteleaf::Site.new(id: site_id)
-  job = site.publish
-  if quiet
-    puts '=> Publish queued.\n'
-  else
-    do_the_job(job)
-    puts '=> Publish completed.\n'
+    def publish(site_id, quiet = true)
+      site = Siteleaf::Site.new(id: site_id)
+      job = site.publish
+      if quiet
+        puts '=> Publish queued.\n'
+      else
+        do_the_job(job)
+        puts '=> Publish completed.\n'
+      end
+    end
   end
 end
