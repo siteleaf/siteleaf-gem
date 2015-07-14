6eb82490d299a027d8dc8e7495f5c1ab9f68c904
amay22 <amay@nyu.edu>
2015-07-14 14:48:29 -0400

    Updated Page Specs and relevant VCR cassettes

--- spec/siteleaf/page_spec.rb
+++ spec/siteleaf/page_spec.rb
@@ -1,59 +1,86 @@
 require File.expand_path('../../spec_helper.rb', __FILE__)
 
 describe 'Page' do
-  let(:page) { Siteleaf::Page.new }
+  # let(:post_id) { nil }
+  let(:id) { nil }
+  let(:parent_id) { nil }
+  let(:site_id) { nil }
+  let(:attributes) do
+    {
+      # post_id: post_id,
+      id: id,
+      parent_id: parent_id,
+      site_id: site_id
+    }
+  end
+  let(:page) { Siteleaf::Page.new(attributes) }
 
   describe '#create_endpoint' do
     subject { page.create_endpoint }
-    context 'page API URL Postfix' do
-      it 'should end with pages' do
-        expect(page.create_endpoint).to end_with('pages')
-      end
+    context 'when site_id is present' do
+      let(:site_id) { 'site_id' }
+      it { should eql 'sites/site_id/pages' }
+    end
+    context 'when site_id is nil' do
+      it { should eql 'sites//pages' }
     end
   end
 
   describe '#site' do
     subject { page.site }
-    context 'Get the Site ID to which that page belongs to' do
-      it 'should return site id' do
-        expect(page.site).to be_nil
-      end
+    context 'when site_id is present' do
+      let(:site_id) { 'site_id' }
+      it { should be_an_instance_of Siteleaf::Site }
+    end
+    context 'when site_id is nil' do
+      it { should eql nil }
     end
   end
 
   describe '#assets', vcr: { cassette_name: 'page_assets', record: :none } do
-    subject { page.assets }
-    context 'Get page assets' do
-      it 'should return an array of page assets' do
-        expect(page.assets).to be_empty
+  subject { page.assets }
+  context 'when id is present and page has assets' do
+    let(:id) { ENV['PAGE_ID'] }
+    it 'should return an array of assets' do
+      page.assets.each do |asset|
+        expect(asset).to be_instance_of(Siteleaf::Asset)
       end
     end
+  end
   end
 
   describe '#posts', vcr: { cassette_name: 'page_posts', record: :none } do
     subject { page.posts }
-    context 'Get page theme using the theme_id' do
-      it 'should return a post or nil if no post declared' do
-        expect(page.posts[0]).to be_instance_of(Siteleaf::Post)
+    context 'when id is present and page has posts' do
+      let(:id) { ENV['PAGE_ID'] }
+      it 'should return an array of posts' do
+        page.posts.each do |post|
+          expect(post).to be_instance_of(Siteleaf::Post)
+        end
       end
     end
   end
 
   describe '#pages', vcr: { cassette_name: 'page_pages', record: :none } do
     subject { page.pages }
-    context 'Get page site using the site_id' do
-      it 'should return a post or nil if no post declared' do
-        expect(page.pages[0]).to be_instance_of(Siteleaf::Page)
+    context 'when id is present and page has subpages' do
+      let(:id) { ENV['PAGE_ID'] }
+      it 'should return an array of pages' do
+        page.pages.each do |page|
+          expect(page).to be_instance_of(Siteleaf::Page)
+        end
       end
     end
   end
 
   describe '#page' do
     subject { page.page }
-    context 'Get page parent id' do
-      it 'should id for the parent page' do
-        expect(page.page).to be_nil
-      end
+    context 'when parent_id is present' do
+      let(:parent_id) { 'parent_id' }
+      it { should be_an_instance_of Siteleaf::Page }
+    end
+    context 'when parent_id is nil' do
+      it { should eql nil }
     end
   end
 end
