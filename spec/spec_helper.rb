390be480363b1a7e17b4c1d33df9453c6fc3d67b
amay22 <amay@nyu.edu>
2015-07-14 10:29:59 -0400

    Added Figs Environment Vars for RSpec Testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,11 @@ VCR.configure do |config|
   config.cassette_library_dir     = 'spec/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.filter_sensitive_data('KEY') { ENV['API_KEY'] } unless ENV['API_KEY'].nil?
+  config.filter_sensitive_data('SECRET') { ENV['API_SECRET'] } unless ENV['API_SECRET'].nil?
+  config.filter_sensitive_data('ID') { ENV['SITELEAF_ID'] } unless ENV['SITELEAF_ID'].nil?
+  config.filter_sensitive_data('ID') { ENV['PAGE_ID'] } unless ENV['PAGE_ID'].nil?
+  config.filter_sensitive_data('ID') { ENV['POST_ID'] } unless ENV['POST_ID'].nil?
+  config.filter_sensitive_data('ID') { ENV['SUBPAGE_ID'] } unless ENV['SUBPAGE_ID'].nil?
   # config.debug_logger = File.open('vcr_errors', 'w')
 end
