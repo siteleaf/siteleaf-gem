b0d88a722a57de25fcebf7f54a9c7f1dce07824b
amay22 <amay@nyu.edu>
2015-07-21 12:16:48 -0400

    Updated Specs for Entity

---
+++ spec/siteleaf/entity_spec.rb
@@ -0,0 +1,149 @@
+require File.expand_path('../../spec_helper.rb', __FILE__)
+
+describe 'Entity' do
+  let(:id) { nil }
+  let(:attributes) { { id: id } }
+  let(:entity) { Siteleaf::Entity }
+  let(:asset) { Siteleaf::Asset }
+  let(:page) { Siteleaf::Page }
+  let(:post) { Siteleaf::Post }
+  let(:theme) { Siteleaf::Theme }
+  let(:site) { Siteleaf::Site }
+  let(:user) { Siteleaf::User }
+
+  describe '#initialize' do
+    subject { entity.new }
+    context 'when entity is initialized' do
+      it { should be_an_instance_of Siteleaf::Entity }
+    end
+  end
+
+  describe '#self.all' do
+    before do
+      stub_request(:get, %r{\/posts\z}).to_return(body: ENV['posts'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{\/pages\z}).to_return(body: ENV['pages'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{\/assets\z}).to_return(body: ENV['assets'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{\/users\z}).to_return(body: ENV['users'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{\/sites\z}).to_return(body: ENV['sites'], headers: { content_type: 'application/json' })
+    end
+    subject(:post_all) { post.all }
+    subject(:page_all) { page.all }
+    subject(:asset_all) { asset.all }
+    subject(:user_all) { user.all }
+    subject(:site_all) { site.all }
+    it 'returns array of Siteleaf objects of that instance' do
+      expect(post_all).to be_an_instance_of Array
+      expect(page_all).to be_an_instance_of Array
+      expect(asset_all).to be_an_instance_of Array
+      expect(user_all).to be_an_instance_of Array
+      expect(site_all).to be_an_instance_of Array
+      post_all.each { |post| expect(post).to be_an_instance_of Siteleaf::Post }
+      page_all.each { |page| expect(page).to be_an_instance_of Siteleaf::Page }
+      asset_all.each { |asset| expect(asset).to be_an_instance_of Siteleaf::Asset }
+      user_all.each { |user| expect(user).to be_an_instance_of Siteleaf::User }
+      site_all.each { |site| expect(site).to be_an_instance_of Siteleaf::Site }
+    end
+  end
+
+  describe '#self.find' do
+    subject(:post_find) { post.find(ENV['POST_ID']) }
+    subject(:page_find) { page.find(ENV['PAGE_ID']) }
+    subject(:asset_find) { asset.find(ENV['ASSET_ID']) }
+    subject(:user_find) { user.find(ENV['USER_ID']) }
+    subject(:site_find) { site.find(ENV['SITE_ID']) }
+    it 'returns a Siteleaf object of that instance if parameter is passed' do
+      expect(post_find).to be_an_instance_of Siteleaf::Post
+      expect(page_find).to be_an_instance_of Siteleaf::Page
+      expect(asset_find).to be_an_instance_of Siteleaf::Asset
+      expect(user_find).to be_an_instance_of Siteleaf::User
+      expect(site_find).to be_an_instance_of Siteleaf::Site
+    end
+  end
+
+  # describe '#self.create' do
+  #   context '' do
+  #     it '' do
+  #       # puts page.create
+  #     end
+  #     # pending
+  #   end
+  # end
+
+  # describe '#save' do
+  #   subject { entity.save }
+  #   context '' do
+  #     # pending
+  #   end
+  # end
+
+  # describe '#self.delete' do
+  #   subject { entity.save }
+  #   context '' do
+  #     # pending
+  #   end
+  # end
+
+  # describe '#delete' do
+  #   subject { entity.save }
+  #   context '' do
+  #     # pending
+  #   end
+  # end
+
+  describe '#attributes' do
+    subject { entity.new.attributes }
+    context 'when Entity is initialized a Hash is made for their attributes' do
+      # No Need to check this function for the other objects as it just returns the same output
+      it { should eql({}) }
+    end
+  end
+
+  describe '#attributes=' do
+    subject { entity.new.attributes = attributes }
+    context 'when Entity is initialized it is allocated the attributes passed .new(attributes)' do
+      # No Need to check this function for the other objects as it just returns the same output
+      it { should eql attributes }
+    end
+  end
+
+  describe '#self.class_name' do
+    subject(:entity_class_name) { entity.class_name }
+    subject(:post_class_name) { post.class_name }
+    subject(:page_class_name) { page.class_name }
+    subject(:asset_class_name) { asset.class_name }
+    subject(:user_class_name) { user.class_name }
+    subject(:site_class_name) { site.class_name }
+    subject(:theme_class_name) { theme.class_name }
+    it 'returns Class name of Object' do
+      expect(entity_class_name).to match 'Entity'
+      expect(post_class_name).to match 'Post'
+      expect(page_class_name).to match 'Page'
+      expect(asset_class_name).to match 'Asset'
+      expect(user_class_name).to match 'User'
+      expect(site_class_name).to match 'Site'
+      expect(theme_class_name).to match 'Theme'
+    end
+  end
+
+  describe '#self.endpoint' do
+    subject(:entity_endpoint) { entity.endpoint }
+    subject(:user_endpoint) { user.endpoint }
+    subject(:theme_endpoint) { theme.endpoint }
+    it 'adds s to lower case of class name' do
+      expect(entity_endpoint).to match 'entitys'
+      expect(user_endpoint).to match 'users'
+      expect(theme_endpoint).to match 'themes'
+    end
+  end
+
+  describe '#self.create_endpoint' do
+    subject(:entity_create_endpoint) { entity.new.create_endpoint }
+    subject(:user_create_endpoint) { user.new.create_endpoint }
+    subject(:theme_create_endpoint) { theme.new.create_endpoint }
+    it 'adds s to lower case of class name' do
+      expect(entity_create_endpoint).to match 'entitys'
+      expect(user_create_endpoint).to match 'users'
+      expect(theme_create_endpoint).to match 'themes'
+    end
+  end
+end
