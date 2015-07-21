1ddcaf1cd9ad54df5dcc109fb0873e8c0e797cbf
amay22 <amay@nyu.edu>
2015-07-21 12:15:47 -0400

    Updated Specs for page and post to use mocks and not VCR

--- spec/siteleaf/page_spec.rb
+++ spec/siteleaf/page_spec.rb
@@ -4,20 +4,14 @@ describe 'Page' do
   let(:id) { nil }
   let(:parent_id) { nil }
   let(:site_id) { nil }
-  let(:attributes) do
-    {
-      id: id,
-      parent_id: parent_id,
-      site_id: site_id
-    }
-  end
+  let(:attributes) { {  id: id, parent_id: parent_id, site_id: site_id } }
   let(:page) { Siteleaf::Page.new(attributes) }
 
   describe '#create_endpoint' do
     subject { page.create_endpoint }
     context 'when site_id is present' do
-      let(:site_id) { ENV['SITELEAF_ID'] }
-      it { should eql 'sites/SITE_ID/pages' }
+      let(:site_id) { ENV['SITE_ID'] }
+      it { should eql "sites/#{ENV['SITE_ID']}/pages" }
     end
     context 'when site_id is nil' do
       it { should eql 'sites//pages' }
@@ -27,7 +21,7 @@ describe 'Page' do
   describe '#site' do
     subject { page.site }
     context 'when site_id is present' do
-      let(:site_id) { ENV['SITELEAF_ID'] }
+      let(:site_id) { ENV['SITE_ID'] }
       it { should be_an_instance_of Siteleaf::Site }
     end
     context 'when site_id is nil' do
@@ -35,39 +29,70 @@ describe 'Page' do
     end
   end
 
-  describe '#assets', vcr: { cassette_name: 'page_assets', record: :none } do
+  describe '#assets' do
+    before do
+      stub_request(:get, %r{pages\/#{ENV['PAGE_ID']}\/assets\z}).to_return(body: ENV['assets'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{pages\/\/assets\z}).to_return(body: ENV['error'], headers: { content_type: 'application/json' })
+    end
     subject { page.assets }
     context 'when id is present' do
       let(:id) { ENV['PAGE_ID'] }
+      it { should be_instance_of Array }
       it 'should return an array of assets' do
-        page.assets.each do |asset|
-          expect(asset).to be_instance_of(Siteleaf::Asset)
-        end
+        page.assets.each { |asset| expect(asset).to be_instance_of Siteleaf::Asset }
+      end
+    end
+    context 'when id is nil' do
+      it 'should raise error for each_pair function in attributes function' do
+        expect { page.assets }.to raise_error NoMethodError
       end
     end
   end
 
-  describe '#posts', vcr: { cassette_name: 'page_posts', record: :none } do
+  describe '#posts' do
+    before do
+      stub_request(:get, %r{pages\/#{ENV['PAGE_ID']}\/posts\z}).to_return(body: ENV['posts'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{pages\/\/posts\z}).to_return(body: ENV['error'], headers: { content_type: 'application/json' })
+    end
     subject { page.posts }
     context 'when id is present' do
       let(:id) { ENV['PAGE_ID'] }
+      it { should be_instance_of Array }
       it 'should return an array of posts' do
-        page.posts.each do |post|
-          expect(post).to be_instance_of(Siteleaf::Post)
-        end
+        page.posts.each { |post| expect(post).to be_instance_of Siteleaf::Post }
+      end
+    end
+    context 'when id is nil' do
+      it 'should raise error for each_pair function in attributes function' do
+        expect { page.posts }.to raise_error NoMethodError
       end
     end
   end
 
-  describe '#pages', vcr: { cassette_name: 'page_pages', record: :none } do
-    subject { page.pages[0] }
+  describe '#pages' do
+    before do
+      stub_request(:get, %r{pages\/#{ENV['PAGE_ID']}\?include=pages\z}).to_return(body: ENV['page'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{pages\/\?include=pages\z}).to_return(body: ENV['error'], headers: { content_type: 'application/json' })
+    end
+    subject { page.pages }
     context 'when id is present' do
       let(:id) { ENV['PAGE_ID'] }
-      it { should be_an_instance_of Siteleaf::Page }
+      it { should be_instance_of Array }
+      it 'should return an array of pages' do
+        page.pages.each { |page| expect(page).to be_instance_of Siteleaf::Page }
+      end
+    end
+    context 'when id is nil' do
+      it 'should raise error for each_pair function in attributes function' do
+        expect { page.pages }.to raise_error TypeError
+      end
     end
   end
 
   describe '#page' do
+    before do
+      stub_request(:get, %r{/pages\z}).to_return(body: ENV['pages'], headers: { content_type: 'application/json' })
+    end
     subject { page.page }
     context 'when parent_id is present' do
       let(:parent_id) { ENV['PARENT_ID'] }
--- spec/siteleaf/post_spec.rb
+++ spec/siteleaf/post_spec.rb
@@ -3,19 +3,14 @@ require File.expand_path('../../spec_helper.rb', __FILE__)
 describe 'Post' do
   let(:id) { nil }
   let(:parent_id) { nil }
-  let(:attributes) do
-    {
-      id: id,
-      parent_id: parent_id
-    }
-  end
+  let(:attributes) { { id: id, parent_id: parent_id } }
   let(:post) { Siteleaf::Post.new(attributes) }
 
   describe '#create_endpoint' do
     subject { post.create_endpoint }
     context 'when parent_id is present' do
       let(:parent_id) { ENV['PARENT_ID'] }
-      it { should eql 'pages/PARENT_ID/posts' }
+      it { should eql "pages/#{ENV['PARENT_ID']}/posts" }
     end
     context 'when parent_id is nil' do
       it { should eql 'pages//posts' }
@@ -24,19 +19,33 @@ describe 'Post' do
 
   describe '#parent' do
     subject { post.parent }
-    context 'Must always return a parent Object' do
+    context 'when parent_id is present' do
+      let(:parent_id) { ENV['PARENT_ID'] }
       it { should be_an_instance_of Siteleaf::Page }
+    end
+    context 'when parent_id is nil' do
+      it 'should raise error for each_pair function in attributes function' do
+        expect { post.assets }.to raise_error NoMethodError
+      end
     end
   end
 
-  describe '#assets', vcr: { cassette_name: 'post_assets', record: :none } do
+  describe '#assets' do
+    before do
+      stub_request(:get, %r{posts\/#{ENV['POST_ID']}\/assets\z}).to_return(body: ENV['assets'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{posts\/\/assets\z}).to_return(body: ENV['error'], headers: { content_type: 'application/json' })
+    end
     subject { post.assets }
     context 'when id is present' do
       let(:id) { ENV['POST_ID'] }
+      it { should be_an_instance_of Array }
       it 'should return an array of assets' do
-        post.assets.each do |asset|
-          expect(asset).to be_instance_of(Siteleaf::Asset)
-        end
+        post.assets.each { |asset| expect(asset).to be_instance_of(Siteleaf::Asset) }
+      end
+    end
+    context 'when id is nil' do
+      it 'should raise error for each_pair function in attributes function' do
+        expect { post.assets }.to raise_error NoMethodError
       end
     end
   end
