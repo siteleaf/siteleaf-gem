df7d1a1170ec5b6e1564ef877286b41cae6f9ec7
amay22 <amay@nyu.edu>
2015-07-21 12:11:29 -0400

    Removed VCR config cassetes and gemspec

--- siteleaf.gemspec
+++ siteleaf.gemspec
@@ -23,8 +23,6 @@ Gem::Specification.new do |gem|
   gem.add_development_dependency 'figs'
   # Added for RSpec testing
   gem.add_development_dependency 'rspec'
-  # To record cassettes for offline testing
-  gem.add_development_dependency 'vcr'
   # To mock a fake web for HTTP requests
   gem.add_development_dependency 'webmock'
   # For code coverage analysis of RSpec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../../lib/siteleaf.rb', __FILE__)
 Dir['/lib/*.rb'].each { |file| require file }
 require 'rspec'
-require 'vcr'
 require 'webmock/rspec'
 require 'simplecov'
 SimpleCov.start
@@ -14,11 +13,4 @@ RSpec.configure do |_c|
   ENV['POST_ID'] ||= 'POST_ID'
   ENV['SUBPAGE_ID'] ||= 'SUBPAGE_ID'
   ENV['PARENT_ID'] ||= 'PARENT_ID'
-end
-
-VCR.configure do |config|
-  config.default_cassette_options = { record: :new_episodes, match_requests_on: [:path] }
-  config.cassette_library_dir     = 'spec/vcr_cassettes'
-  config.hook_into :webmock
-  config.configure_rspec_metadata!
 end
