cc58b0f25a591a0165b25fa352138822270a84ad
amay22 <amay@nyu.edu>
2015-07-21 12:14:24 -0400

    Updated Specs for Asset

--- spec/siteleaf/asset_spec.rb
+++ spec/siteleaf/asset_spec.rb
@@ -5,34 +5,27 @@ describe 'Asset' do
   let(:theme_id) { nil }
   let(:page_id) { nil }
   let(:site_id) { nil }
-  let(:attributes) do
-    {
-      post_id: post_id,
-      theme_id: theme_id,
-      page_id: page_id,
-      site_id: site_id
-    }
-  end
+  let(:attributes) { { post_id: post_id, theme_id: theme_id, page_id: page_id, site_id: site_id } }
   let(:asset) { Siteleaf::Asset.new(attributes) }
 
   describe '#create_endpoint' do
     subject { asset.create_endpoint }
     context 'when site_id is present' do
-      let(:site_id) { ENV['SITELEAF_ID'] }
-      it { should eql 'sites/SITE_ID/assets' }
+      let(:site_id) { ENV['SITE_ID'] }
+      it { should eql "sites/#{ENV['SITE_ID']}/assets" }
     end
     context 'when theme_id and site_id is present' do
-      let(:theme_id) { ENV['SITELEAF_ID'] }
-      let(:site_id) { ENV['SITELEAF_ID'] }
-      it { should eql 'sites/SITE_ID/theme/assets' }
+      let(:theme_id) { ENV['SITE_ID'] }
+      let(:site_id) { ENV['SITE_ID'] }
+      it { should eql "sites/#{ENV['SITE_ID']}/theme/assets" }
     end
     context 'when post_id is present' do
       let(:post_id) { ENV['POST_ID'] }
-      it { should eql 'posts/POST_ID/assets' }
+      it { should eql "posts/#{ENV['POST_ID']}/assets" }
     end
     context 'when page_id is present' do
       let(:page_id) { ENV['PAGE_ID'] }
-      it { should eql 'pages/PAGE_ID/assets' }
+      it { should eql "pages/#{ENV['PAGE_ID']}/assets" }
     end
     context 'when page_id, post_id, and theme_id are all nil' do
       it { should eql 'sites//assets' }
@@ -64,7 +57,7 @@ describe 'Asset' do
   describe '#theme' do
     subject { asset.theme }
     context 'when theme_id is present' do
-      let(:theme_id) { ENV['SITELEAF_ID'] }
+      let(:theme_id) { ENV['SITE_ID'] }
       it { should be_an_instance_of Siteleaf::Theme }
     end
     context 'when theme_id is nil' do
@@ -75,7 +68,7 @@ describe 'Asset' do
   describe '#site' do
     subject { asset.site }
     context 'when site_id is present' do
-      let(:site_id) { ENV['SITELEAF_ID'] }
+      let(:site_id) { ENV['SITE_ID'] }
       it { should be_an_instance_of Siteleaf::Site }
     end
     context 'when site_id is nil' do
