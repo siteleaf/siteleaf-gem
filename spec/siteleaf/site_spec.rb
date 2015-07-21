a282b0df4ca3e072152c8ab8b6464f9f95a41e20
amay22 <amay@nyu.edu>
2015-07-21 12:16:22 -0400

    Updated Specs for site and theme to use mocks and not VCR

--- spec/siteleaf/site_spec.rb
+++ spec/siteleaf/site_spec.rb
@@ -12,38 +12,62 @@ describe 'Site' do
     end
   end
 
-  describe '#assets', vcr: { cassette_name: 'site_assets', record: :none } do
+  describe '#assets' do
+    before do
+      stub_request(:get, %r{sites\/#{ENV['SITE_ID']}\/assets\z}).to_return(body: ENV['assets'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{sites\/\/assets\z}).to_return(body: ENV['error'], headers: { content_type: 'application/json' })
+    end
     subject { site.assets }
     context 'when id is present' do
-      let(:id) { ENV['SITELEAF_ID'] }
+      let(:id) { ENV['SITE_ID'] }
+      it { should be_an_instance_of Array }
       it 'should return an array of assets' do
-        site.assets.each do |asset|
-          expect(asset).to be_instance_of(Siteleaf::Asset)
-        end
+        site.assets.each { |asset| expect(asset).to be_instance_of(Siteleaf::Asset) }
+      end
+    end
+    context 'when id is nil' do
+      it 'should raise error for each_pair function in attributes function' do
+        expect { site.assets }.to raise_error NoMethodError
       end
     end
   end
 
-  describe '#pages', vcr: { cassette_name: 'site_pages', record: :none } do
+  describe '#pages' do
+    before do
+      stub_request(:get, %r{sites\/#{ENV['SITE_ID']}\/pages\z}).to_return(body: ENV['pages'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{sites\/\/pages\z}).to_return(body: ENV['error'], headers: { content_type: 'application/json' })
+    end
     subject { site.pages }
     context 'when id is present' do
-      let(:id) { ENV['SITELEAF_ID'] }
+      let(:id) { ENV['SITE_ID'] }
+      it { should be_an_instance_of Array }
       it 'should return an array of pages' do
-        site.pages.each do |page|
-          expect(page).to be_instance_of(Siteleaf::Page)
-        end
+        site.pages.each { |page| expect(page).to be_instance_of(Siteleaf::Page) }
+      end
+    end
+    context 'when id is nil' do
+      it 'should raise error for each_pair function in attributes function' do
+        expect { site.pages }.to raise_error NoMethodError
       end
     end
   end
 
-  describe '#posts', vcr: { cassette_name: 'site_posts', record: :none } do
+  describe '#posts' do
+    before do
+      stub_request(:get, %r{sites\/#{ENV['SITE_ID']}\/posts\z}).to_return(body: ENV['posts'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{sites\/\/posts\z}).to_return(body: ENV['error'], headers: { content_type: 'application/json' })
+    end
     subject { site.posts }
     context 'when id is present' do
-      let(:id) { ENV['SITELEAF_ID'] }
+      let(:id) { ENV['SITE_ID'] }
+      it { should be_an_instance_of Array }
       it 'should return an array of posts' do
-        site.posts.each do |post|
-          expect(post).to be_instance_of(Siteleaf::Post)
-        end
+        site.posts.each { |post|  expect(post).to be_instance_of(Siteleaf::Post) }
+      end
+    end
+    context 'when id is nil' do
+      it 'should raise error for each_pair function in attributes function' do
+        expect { site.posts }.to raise_error NoMethodError
       end
     end
   end
--- spec/siteleaf/theme_spec.rb
+++ spec/siteleaf/theme_spec.rb
@@ -12,32 +12,49 @@ describe 'Theme' do
     end
   end
 
-  describe '#assets', vcr: { cassette_name: 'theme_assets', record: :none } do
+  describe '#assets' do
+    before do
+      stub_request(:get, %r{sites\/#{ENV['SITE_ID']}\/theme\/assets\z}).to_return(body: ENV['assets'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{sites\/\/theme\/assets\z}).to_return(body: ENV['error'], headers: { content_type: 'application/json' })
+    end
     subject { theme.new(attributes).assets }
     context 'when site_id is present' do
-      let(:site_id) { ENV['SITELEAF_ID'] }
+      let(:site_id) { ENV['SITE_ID'] }
+      it { should be_instance_of Array }
       it 'should return an array of assets' do
-        theme.new(attributes).assets.each do |asset|
-          expect(asset).to be_instance_of(Siteleaf::Asset)
-        end
+        theme.new(attributes).assets.each { |asset| expect(asset).to be_instance_of(Siteleaf::Asset) }
+      end
+    end
+    context 'when site_id is nil' do
+      it 'should raise error for each_pair function in attributes function' do
+        expect { theme.new(attributes).assets }.to raise_error NoMethodError
       end
     end
   end
 
-  describe '#assets_by_site_id', vcr: { cassette_name: 'theme_assets_by_site_id', record: :none }  do
-    subject { theme.assets_by_site_id(ENV['SITELEAF_ID']) }
+  describe '#assets_by_site_id' do
+    before do
+      stub_request(:get, %r{sites\/#{ENV['SITE_ID']}\/theme\/assets\z}).to_return(body: ENV['assets'], headers: { content_type: 'application/json' })
+      stub_request(:get, %r{sites\/\/theme\/assets\z}).to_return(body: ENV['error'], headers: { content_type: 'application/json' })
+    end
+    subject { theme.assets_by_site_id(ENV['SITE_ID']) }
     context 'when site_id is passed as parameter' do
+      it { should be_an_instance_of Array }
       it 'should return an array of assets' do
-        theme.assets_by_site_id(ENV['SITELEAF_ID']).each do |asset|
-          expect(asset).to be_instance_of(Siteleaf::Asset)
-        end
+        theme.assets_by_site_id(ENV['SITE_ID']).each { |asset| expect(asset).to be_instance_of(Siteleaf::Asset) }
+      end
+    end
+    context 'when site_id is nil' do
+      it 'should raise error for each_pair function in attributes function' do
+        expect { theme.new(attributes).assets }.to raise_error NoMethodError
       end
     end
   end
 
   describe '#site' do
-    subject { theme.new.site }
-    context 'will always return Site object' do
+    subject { theme.new(attributes).site }
+    context 'when site_id is present' do
+      let(:site_id) { ENV['SITE_ID'] }
       it { should be_an_instance_of Siteleaf::Site }
     end
   end
